d403a8a34a86940590d45002ccb89f89a38e2979
takundachirema <takundachirema@Takundas-MBP.home>
2022-11-08 18:47:47 +0200

    use versel adapter for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,22 @@ import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
 import lit from '@astrojs/lit';
 
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://aarons-kit-website.vercel.app/',
-	// Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-	sitemap: true,
-	// Generate sitemap (set to "false" to disable)
-	integrations: [sitemap(), mdx(), image(), lit()],
-	// Add renderers to the config
-	// This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
-	vite: {
-		ssr: {
-			external: ['svgo'],
-		},
-	},
-	// output: 'server',
-	// adapter: vercel(),
+  output: 'server',
+  site: 'https://odyssey-theme.littlesticks.dev/',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [sitemap(), mdx(), image(), lit()],
+  // Add renderers to the config
+  // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
+  adapter: vercel()
 });
