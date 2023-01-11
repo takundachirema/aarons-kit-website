d23eef9f6e517f9d38567c5e9b6c3e537fbb2629
takundachirema <takundachirema@Takundas-MacBook-Pro.local>
2023-01-11 10:43:34 +0100

    use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
 
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 
